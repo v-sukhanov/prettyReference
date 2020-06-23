3b30a94eb14f1a2ccaafc29c92814f6ac958ef66
v-sukhanov <vladimir.sukhanov99.@gmail.com>
2020-06-23 23:24:43 +0300

    init commit

--- view-angular/src/app/app.module.ts
+++ view-angular/src/app/app.module.ts
@@ -3,20 +3,37 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { LeftBarComponent } from './components/left-bar/left-bar.component';
 import { ContentComponent } from './components/content/content.component';
 import { FlexLayoutModule } from '@angular/flex-layout';
+import { MaterialModule } from './material/material.module';
+import { StoreModule } from '@ngrx/store';
+import { reducer } from './store/reducers/topic.reducer';
+import { StoreDevtoolsModule } from '@ngrx/store-devtools';
+import { environment } from '../environments/environment';
+import { FormsModule } from '@angular/forms';
+import { MatTreeModule } from '@angular/material/tree';
+import { MatIconModule } from '@angular/material/icon';
+import { MatButtonModule } from '@angular/material/button';
+import { PanelComponent } from './components/panel/panel.component';
+import { ColorPickerModule } from 'ngx-color-picker';
 
 @NgModule({
     declarations: [
         AppComponent,
-        LeftBarComponent,
-        ContentComponent
+        ContentComponent,
+        PanelComponent
     ],
     imports: [
         BrowserModule,
         BrowserAnimationsModule,
-        FlexLayoutModule
+        FlexLayoutModule,
+        MaterialModule,
+        FormsModule,
+        StoreModule.forRoot({
+            topics: reducer
+        }),
+        StoreDevtoolsModule.instrument({ maxAge: 25, logOnly: environment.production }),
+        ColorPickerModule
     ],
     providers: [],
     bootstrap: [AppComponent]

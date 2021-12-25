5d08d271674231ce191e9906ca32c2898fe3cd01
frontender <v-sukhanov>
2021-12-25 06:26:31 +0300

    feat(tests): added tost on get site date by rabbit

--- backend/Crawler/PrettyReference.Crawler/PrettyReference.Crawler.Test/UnitTest1.cs
+++ backend/Crawler/PrettyReference.Crawler/PrettyReference.Crawler.Test/UnitTest1.cs
@@ -1,8 +1,10 @@
 using System.Threading.Tasks;
+using MassTransit;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using NUnit.Framework;
 using PrettyReference.Crawler.Core.CrawlerClients;
+using PrettyReference.Crawler.Interface.SabeMetaData;
 using Serilog;
 
 namespace PrettyReference.Crawler.Test
@@ -34,7 +36,18 @@ namespace PrettyReference.Crawler.Test
             var host = await BuildTestHost();
             var crawlerClient = host.ServiceProvider.GetRequiredService<CrawlerClient>();
             var data = crawlerClient.SaveMetaDataByUrl("https://docs.docker.com/compose/reference/");
-            
+            Assert.Pass();
+        }
+        
+        [Test]
+        public async Task GetAndSaveSiteDateByRabbitMq()
+        {
+            var host = await BuildTestHost();
+            var bus = host.ServiceProvider.GetRequiredService<IBusControl>();
+            var response = await bus.Request<SaveMetaDataRequest, SaveMetaDataResponse>(new SaveMetaDataRequest()
+            {
+                Url = "https://docs.docker.com/compose/reference/"
+            });
             Assert.Pass();
         }
     }

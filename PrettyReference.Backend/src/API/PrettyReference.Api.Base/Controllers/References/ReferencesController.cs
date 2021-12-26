9371b04b5c393b723e00b0bf296a7858433e71d6
frontender <v-sukhanov>
2021-12-26 19:42:48 +0300

    feat(front): create front project

--- PrettyReference.Backend/src/API/PrettyReference.Api.Base/Controllers/References/ReferencesController.cs
+++ PrettyReference.Backend/src/API/PrettyReference.Api.Base/Controllers/References/ReferencesController.cs
@@ -4,6 +4,7 @@ using System.Threading.Tasks;
 using MassTransit;
 using Microsoft.AspNetCore.Mvc;
 using PrettyReference.Api.Base.Controllers.References.models;
+using PrettyReference.Crawler.Interface.GetMetaDataList;
 using PrettyReference.Crawler.Interface.SaveMetaData;
 
 namespace PrettyReference.Api.Base.Controllers.References
@@ -11,7 +12,7 @@ namespace PrettyReference.Api.Base.Controllers.References
     public class ReferencesController: Controller
     {
         [HttpPost]
-        public async Task<SiteMetaDataItem> ParseAndSaveMetadata([FromServices] IBusControl busControl, [FromBody] ParseAndSaveMetadataRequest request )
+        public async Task<SiteMetaDataItem> AddUrl([FromServices] IBusControl busControl, [FromBody] ParseAndSaveMetadataRequest request )
         {
             var response = await busControl.Request<SaveMetaDataRequest, SaveMetaDataResponse>(
                 new SaveMetaDataRequest()
@@ -19,6 +20,14 @@ namespace PrettyReference.Api.Base.Controllers.References
                     Url = request.Url
                 });
             return response.Message.Item;
+        }
+        
+        [HttpGet]
+        public async Task<SiteMetaDataItem[]> GetUrlList([FromServices] IBusControl busControl)
+        {
+            var response = await busControl.Request<GetMetaDataListRequest, GetMetaDataListResponse>(
+                new GetMetaDataListRequest());
+            return response.Message.Items;
         }
     }
 }

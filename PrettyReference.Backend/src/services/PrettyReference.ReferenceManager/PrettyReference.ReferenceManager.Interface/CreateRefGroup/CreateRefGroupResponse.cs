a5d9a6751c1dd55787760178c63b6432d0f4c676
frontender <v-sukhanov>
2022-01-11 00:22:37 +0300

    feat(reference): redirect to created group

--- PrettyReference.Backend/src/services/PrettyReference.ReferenceManager/PrettyReference.ReferenceManager.Interface/CreateRefGroup/CreateRefGroupResponse.cs
+++ PrettyReference.Backend/src/services/PrettyReference.ReferenceManager/PrettyReference.ReferenceManager.Interface/CreateRefGroup/CreateRefGroupResponse.cs
@@ -1,7 +1,9 @@
+using PrettyReference.ReferenceManager.Interface.Shared;
+
 namespace PrettyReference.ReferenceManager.Interface.CreateRefGroup
 {
     public class CreateRefGroupResponse
     {
-        
+        public RefGroup Item { get; set; }
     }
 }
